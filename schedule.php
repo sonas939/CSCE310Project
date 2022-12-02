ea05bcfd34e12d6dc5c9c045d8d2c9e4eff816af
jwgrimes67 <jwgrimes67@gmail.com>
2022-12-02 15:54:11 -0600

    Made schedule different per user

--- schedule.php
+++ schedule.php
@@ -1,5 +1,7 @@
 <title>Build A Bread</title>
-
+<?php
+session_start();
+?>
 <head>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -71,18 +73,22 @@
                 
                 return $conn;
               }
+            if (isset($_POST['admin_back'])) {
+                // redirect to create account
+                header("Location: /admin.php");
+            } 
 
-              // connect to db and query profile information
+              // connect to database to get schedule view
               $conn = getDB();
               $sql = "SELECT * FROM `view_schedules`";
               /*CREATE VIEW `view_schedules` AS
-	                SELECT A.order_id, start_time, end_time, A.order_status
+	                SELECT A.order_id, start_time, end_time, A.order_status, A.profile_id
 		            FROM `orders` A, `schedules` B
 		            WHERE A.order_id = B.order_id
             */
               $res = $conn->query($sql);
 
-              // begin profile table
+              // begin schedule table
               echo '<form method="post" action="schedule.php">';
               echo '<div class="container table-responsive">';
               echo '<table class="table table-bordered">';
@@ -96,26 +102,27 @@
               echo '</thead>';
               echo '<tbody>';
 
-              $uIDs = [];
+              $schedule = [];
 
               // iterate through each profile, determine account type, display in table
               while ($row = mysqli_fetch_array($res)) {
                 // all admins must be employees
 
-                array_push($uIDs, $row['order_id']);
-
-                echo "<tr>";
-                echo "<td>" . $row['order_id'] . "</td>";
-                echo "<td>" . date("m/d/y g:i A", strtotime($row['start_time'])) . "</td>";
-                echo "<td>" . date("m/d/y g:i A", strtotime($row['end_time'])) . "</td>";
-                if ($row['order_status'] == 1) {
-                    echo "<td>" . 'In Progress'. "</td>";
-                } elseif ($row['order_status'] == 2) {
-                    echo "<td>" . 'Fulfilled'. "</td>";
-                } else {
-                    echo "<td>" . 'Canceled'. "</td>";
+                array_push($schedule, $row['order_id']);
+                if ($_SESSION['user_type'] == 3 or $_SESSION['user_type'] == 2 or $_SESSION['profile_id'] == $row['profile_id']) {
+                    echo "<tr>";
+                    echo "<td>" . $row['order_id'] . "</td>";
+                    echo "<td>" . date("m/d/y g:i A", strtotime($row['start_time'])) . "</td>";
+                    echo "<td>" . date("m/d/y g:i A", strtotime($row['end_time'])) . "</td>";
+                    if ($row['order_status'] == 1) {
+                        echo "<td>" . 'In Progress' . "</td>";
+                    } elseif ($row['order_status'] == 2) {
+                        echo "<td>" . 'Fulfilled' . "</td>";
+                    } else {
+                        echo "<td>" . 'Canceled' . "</td>";
+                    }
+                    echo "</tr>";
                 }
-                echo "</tr>";
               }
 
               // end profile table
@@ -124,6 +131,10 @@
 
               $conn->close();
             ?>
+            <form class = "form-signin" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> " method = "post">
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit"
+                name = "admin_back">Return to Profile Page</button> 
+            </form>
             <!-- End Code from John Grimes -->
         </div>
     </div> 
