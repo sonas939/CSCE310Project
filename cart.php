8ada821d86bb15026396f880a46f9f2732a7d1bc
sonas939 <70652191+sonas939@users.noreply.github.com>
2022-12-04 08:13:14 -0600

    updated order page

---
+++ cart.php
@@ -0,0 +1,88 @@
+<?php
+    session_start();
+    require_once("connection.php");
+    require_once("component.php");
+
+    if (isset($_POST['remove'])) {
+        if ($_GET['action'] == 'remove') {
+            foreach ($_SESSION['cart'] as $key => $value) {
+                if ($value['product_id'] == $_GET['id']) {
+                    unset($_SESSION['cart'][$key]);
+                    echo "<script>alert('Product has been Removed!')</script>";
+                    echo "<script>window.location='cart.php'</script>";
+                }
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="bg-light">
+    <?php require_once("header.php"); ?>
+    
+    <div class="container-fluid">
+        <div class="row px-5">
+            <div class="col">
+                <div class="shopping-cart">
+                    <h6>My Cart</h6>
+                    <hr>
+                    <?php
+                        $total=0;
+                        if (isset($_SESSION['cart'])) {
+                            $product_id = array_column($_SESSION['cart'],'product_id'); 
+                            $sql = "SELECT * FROM `Items`"; 
+                            $result = $conn->query($sql);
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                foreach($product_id as $id) {
+                                     if($row['item_id'] == $id) {
+                                        cartElement($row['item_name'],$row['item_price'],$row['item_description'],$id);
+                                        $total = $total + (int)$row['item_price'];
+                                     }
+                                }
+                            }
+                        } else {
+                            echo "<h5>Cart is Empty</h5>";
+                        }                       
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-3 offset-md-1 border rounded mt-5 bg-white h-25">
+
+            <div class="pt-4">
+                <h6>PRICE DETAILS</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                            if (isset($_SESSION['cart'])){
+                                $count  = count($_SESSION['cart']);
+                                echo "<h6>Total ($count items)</h6>";
+                            }else{
+                                echo "<h6>Price (0 items)</h6>";
+                            }
+                        ?>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>$<?php echo $total; ?></h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,126 +1,63 @@
 <?php
-   ob_start();
-   session_start();
-?>
+    //start session
+    session_start();
 
-<?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
-?>
+    require_once('connection.php');
+    require_once('component.php');
 
-<html lang = "en">
-   
-   <head>
-      <title>Build-A-Bread</title>
-      <link href = "css/bootstrap.min.css" rel = "stylesheet">
-      
-      <style>
-         body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #ADABAB;
-         }
-         
-         .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: #017572;
-         }
-         
-         .form-signin .form-signin-heading,
-         .form-signin .checkbox {
-            margin-bottom: 10px;
-         }
-         
-         .form-signin .checkbox {
-            font-weight: normal;
-         }
-         
-         .form-signin .form-control {
-            position: relative;
-            height: auto;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
-         }
-         
-         .form-signin .form-control:focus {
-            z-index: 2;
-         }
-         
-         .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-            border-color:#017572;
-         }
-         
-         .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-            border-color:#017572;
-         }
-         
-         h1{
-            text-align: center;
-            color: #017572;
-         }
-      </style>
-      
-   </head>
-	
-   <body>
-      <h1>Welcome to Build-A-Bread</h1> 
-      <div class = "container form-signin">
-         
-         <?php
-            $msg = '';
-            
-            if (isset($_POST['create_account'])) {
-               // redirect to create account
-               header("Location: /create_account_front.php");
+    if (isset($_POST['add'])) {
+        if(isset($_SESSION['cart'])) {
+            $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+            if(in_array($_POST['product_id'], $item_array_id)){
+                echo "<script>alert('Product is already added in the cart!')</script>";
+                echo "<script>window.location = 'index.php'</script>";
+            } else {
+               $count = count($_SESSION['cart']);
+               $item_array = array(
+                    'product_id'=> $_POST['product_id']
+                );
+                $_SESSION['cart'][$count] = $item_array;
+                print_r($_SESSION['cart']);
             }
-            elseif (isset($_POST['edit_account'])) {
-               // redirect to create account
-               header("Location: /update_account_front.php");
-            }         
-         ?>
-      </div> <!-- /container -->
-      
-      <div class = "container">
-      
-         <!-- log in form START -->
-         <form class = "form-signin" action="/login_back.php">
-         <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-         <!-- <label for="username">username:</label> -->
-         <input type="text" id="username" name="username" class = "form-control" placeholder = "Username:" Required><br>
-         <!-- <label for="password">password:</label> -->
-         <input type="password" id="password" name="password" class = "form-control" placeholder = "Password:" Required>
-         <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Login</button>
-            
-         </form>
-         <!-- log in form END -->
-      
-         <!-- Edit and Create accounts: -->
-         <form class = "form-signin" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> " method = "post">
-         <button class = "btn btn-lg btn-primary btn-block" type = "submit"
-               name = "create_account">Create Account</button> 
-         <button class = "btn btn-lg btn-primary btn-block" type = "submit"
-            name = "edit_account">Edit Account</button> 
-         </form>
+        } else {
+            //create new array if session has not be set
+            $item_array = array(
+                'product_id'=> $_POST['product_id']
+            );
 
-         <a href = "logout.php" tite = "Logout">Log Out</a>
-         
-      </div> 
-      <img src="/loaf.png" width="280" height="125" title="lovely bread" alt="Loving bread" class="center"/>
-      <img src="/breadBox.jpg" width="280" height="125" title="Nichijou Bread" alt="Nichijou Bread" class="center"/>
-      <img src="/loaf.png" width="280" height="125" title="lovely bread" alt="Loving bread" class="center"/>
-      <img src="/breadBox.jpg" width="280" height="125" title="Nichijou Bread" alt="Nichijou Bread" class="center"/>
-      <img src="/loaf.png" width="280" height="125" title="lovely bread" alt="Loving bread" class="center"/>
-
-   </body>
+            //create new session variable
+            $_SESSION['cart'][0] = $item_array;
+            print_r($_SESSION['cart']);
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php require_once("header.php");?>
+    <div class="container">
+        <div class="row text-center py-5">
+            <?php
+                $sql = "SELECT * FROM `Items`"; 
+                $result = $conn->query($sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    component($row['item_name'],$row['item_description'],$row['item_price'],$row['item_id']);
+                }
+            ?>
+        </div>
+    </div>
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+</body>
 </html>
