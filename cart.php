b04bf4777bd2ae5c8611b5205c826f1f4b512eef
sonas939 <70652191+sonas939@users.noreply.github.com>
2022-12-04 21:16:18 -0600

    Updated cart.php

--- cart.php
+++ cart.php
@@ -8,19 +8,92 @@
             foreach ($_SESSION['cart'] as $key => $value) {
                 if ($value['product_id'] == $_GET['id']) {
                     unset($_SESSION['cart'][$key]);
-                    echo "<script>alert('Product has been Removed!')</script>";
-                    echo "<script>window.location='cart.php'</script>";
                 }
             }
         }
     }
 
     if (isset($_POST['checkout'])) {
-        foreach ($_SESSION['cart'] as $key => $value) {
-            unset($_SESSION['cart'][$key]);
+        //make sure there are items in the cart. If not alert
+        if (isset($_SESSION['cart'])) {
+            $count  = count($_SESSION['cart']);
+            if ($count == 0) {
+                echo "<script>alert('There are no items in your cart!')</script>";
+                echo "<script>window.location='cart.php'</script>";
+            }
+        } else {
+            echo "<script>alert('There are no items in your cart!')</script>";
+            echo "<script>window.location='cart.php'</script>";
         }
-        echo "<script>alert('You have checked-out!')</script>";
-        echo "<script>window.location='order.php'</script>";
+
+        $profile_id = $_SESSION['profile_id'];
+        $comment_field = $_REQUEST['comment'];
+
+        if (strlen(trim($comment_field))) {
+            print_r($comment_field);
+            // add to database
+        }
+        
+        //error checking for date
+        $date = $_REQUEST['date'];
+        $s_time = $_REQUEST['start-time'];
+        $e_time = $_REQUEST['end-time'];
+
+        if (!strlen(trim($date)) && validateDate($date)) {
+            echo "<script>alert('Please enter a Valid Pick-Up Date!')</script>";
+            echo "<script>window.location='cart.php'</script>";
+        }
+
+        if (!strlen(trim($s_time)) && isTimeValid($s_time)) {
+            echo "<script>alert('Please enter a Valid Start Time!')</script>";
+            echo "<script>window.location='cart.php'</script>";
+        }
+
+        if (!strlen(trim($e_time) && isTimeValid($e_time))) {
+            echo "<script>alert('Please enter a Valid End Time!')</script>";
+            echo "<script>window.location='cart.php'</script>";
+        }
+
+        //add schedule to table
+        $full_start_time = $date . ' ' . date("H:i", strtotime($s_time));
+        $full_end_time = $date . ' ' . date("H:i", strtotime($e_time));
+        //add schedule to table 
+
+        $sql = "INSERT INTO Schedules(start_time, end_time) VALUES ('$full_start_time', '$full_end_time')";
+        if ($conn->query($sql) === FALSE) {
+            echo "Failed to Check-Out";
+        }
+
+        //get last inserted schedule_id
+        $sql = "SELECT LAST_INSERT_ID()";
+        $result = $conn->query($sql);
+        $schedule_id = $conn->insert_id;
+
+        //get total for order table
+        $total = 0;
+        $product_id = array_column($_SESSION['cart'],'product_id'); 
+        $sql = "SELECT * FROM `Items`"; 
+        $result = $conn->query($sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            foreach($product_id as $id) {
+                if($row['item_id'] == $id) {
+                    $total = $total + $row['item_price'];
+                }
+            }
+        }
+
+        //add order to table
+        $sql = "INSERT INTO Orders(price_total, schedule_id, profile_id, order_status) VALUES ('$total', '$schedule_id', '$profile_id', '1')";
+        if ($conn->query($sql) === FALSE) {
+            echo "Failed to Check-Out";
+        }
+
+        /*foreach ($_SESSION['cart'] as $key => $value) {
+            unset($_SESSION['cart'][$key]);
+        }*/
+
+        //echo "<script>alert('You have checked-out!')</script>";
+        //echo "<script>window.location='order.php'</script>";
     }
 ?>
 
@@ -65,9 +138,21 @@
                 </div>
                 <form action="cart.php" method="post">
                     <div class="form-group">
+                    <h6>Enter your desired pick-up time. Can only pick-up during hours of operation (Every day 7AM-7PM)</h6>
+                    <hr>
+                    <textarea class="form-control" rows="1" name="date" placeholder="Enter Pick-Up date (YYYY-MM-DD)"></textarea>
+                    <textarea class="form-control" rows="1" name="start-time" placeholder="Enter Pick-up Start Window (##:## AM or ##:## PM)"></textarea>
+                    <textarea class="form-control" rows="1" name="end-time" placeholder="Enter Pick-up End Window (##:## AM or ##:## PM)"></textarea>
+                    </div>
+    
+                    <div class="form-group">
                     <h6>Enter any commments or special requests.</h6>
                     <hr>
-                    <textarea class="form-control" rows="5" id="comment" name="comment"></textarea>
+                    <textarea class="form-control" rows="5" name="comment"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                    <button type="submit" class="btn btn-warning mx-2" name="checkout">Check Out</button>
                     </div>
                 </form>
             </div>
@@ -79,22 +164,12 @@
                 <div class="row price-details">
                     <div class="col-md-6">
                         <?php
-                            if (isset($_SESSION['cart'])){
-                                $count  = count($_SESSION['cart']);
-                                echo "<h6>Total ($count items)</h6>";
-                            }else{
-                                echo "<h6>Price (0 items)</h6>";
-                            }
+                            echo "<h6>Total ($count items)</h6>";
                         ?>
                     </div>
                     <div class="col-md-6">
                         <h6>$<?php echo $total; ?></h6>
                     </div>
-                </div>
-                <div class="cart-checkout">
-                    <form action="cart.php" method="post">
-                        <button type="submit" class="btn btn-warning mx-2" name="checkout">Check Out</button>
-                    </form>
                 </div>
             </div>
         </div>
